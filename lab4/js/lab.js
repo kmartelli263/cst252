759c63c3f8b498cc70f5362ee4100da61cf12272
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-09-13 21:01:44 -0700

    lab 4 edits

---
+++ lab4/js/lab.js
@@ -0,0 +1,6 @@
+//pseudocode for fixing a flat tire on a bike <br/>
+  //1. Take the wheel off the bike <br/>
+  //2. Remove the tube from the tire <br/>
+  //3. Find the cause of the flat <br/>
+  //4. Patch the tube or replace it <br/>
+  //5. Reinstall the wheel ont he bike <br/>
