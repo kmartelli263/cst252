51ac9554651fa2f406febc93a028e597c00dcc89
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-09-16 11:04:32 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Kevin Martelli <kmartelli@csumb.edu>
+* Created: 16 September 2020
+* License: Public Domain
+*/
+
+// Define variables
+make = "Toyota";
+model = "4Runner";
+color = "Silver";
+year = 1999;
+
+
+// Calculate
+age = 2020 - year;
+
+// Output
+var make = document.writeln("Make: " + make + "<br>");
+var model = document.writeln("Model: " + model + "<br>");
+var color = document.writeln("Color: " + color + "<br>");
+var year = document.writeln("Year: " + year + "<br>");
+var age = document.writeln("Age: " + age + "<br>");
