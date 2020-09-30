b8552f9e62b9d2ff4d5b99a71a71694993a7465c
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-09-29 20:53:34 -0700

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,15 @@
+/*  Author: Kevin Martelli <kmartelli@csumb.edu
+ *  Created: 29 September 2020
+ *  License: Public Domain
+ *
+ */
+
+ // Finding an output and defining a variable
+ var outputE1 =
+    document.getElementByID("div");
+ var new1El =
+    document.createElement("p")
+ var new2El =
+    document.createElement("head")
+ targetE1.appendChild(new1El);
+ targetE1.appendChild(new2El);
