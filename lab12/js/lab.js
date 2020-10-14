d1c7dd0cf026a63ce9f4191544ae020eed06aeff
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-10-13 21:21:17 -0700

    lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,12 +1,29 @@
-for (i=1; i <= 200; i++) {
-    if (i%15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i%3 == 0) {
-        console.log("Fizz");
-    } else if (i%5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
+console.log("There is no multiple of 3, 5, and 7 until the number 105");
+for (var i=1; i<201; i++) {
+    console.log(i);
 
-  }
+      if ((i%5==0)&&(i%3==0)&&(i%7==0)) {
+        console.log("fizz buzz boom");
+        document.getElementById('output').innerHTML += "fizz buzz boom" + "<br>";
+
+      } else if((i%5==0)&&(i%3==0)) {
+        console.log("fizz buzz");
+        document.getElementById('output').innerHTML += "fizz buzz" + "<br>";
+
+      } else if(i%7==0) {
+        console.log("boom");
+        document.getElementById('output').innerHTML += "boom" + "<br>";
+
+      } else if(i%3==0) {
+        console.log("fizz");
+        document.getElelementById('output').innerHTML += "fizz" + "<br>";
+
+      } else if(i%5==0) {
+        console.log("buzz");
+        document.getElelementById('output').innerHTML += "buzz" + "<br>";
+
+      } else if((i%5!==0)&&(i%3!==0)&&(i%7!==0)) {
+        console.log("not a multiple of 3, 5, or 7");
+        document.getElementById('output').innerHTML += "not a multiple of 3, 5, or 7" + "<br>";
+      }
+}
