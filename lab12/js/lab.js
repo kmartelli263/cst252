bb6e9bcf70ea38cc4429218edafe172cdda457a1
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-10-26 18:27:57 -0700

    Update lab.js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,3 +1,47 @@
+if (i%105 ==0) {
+      newEl.innerHTML = i + " - FizzBuzzBoom";
+      outputEl.appendChild(newEl);
+      console.log("FizzBuzzBoom");
+
+    } else if (i%35 ==0) {
+      newEl.innerHTML = i + " - BoomBuzz";
+      outputEl.appendChild(newEl);
+      console.log("BoomBuzz");
+
+    } else if (i%21 ==0) {
+      newEl.innerHTML = i + " - FizzBoom";
+      outputEl.appendChild(newEl);
+      console.log("FizzBoom");
+
+    } else if (i%15 ==0) {
+      newEl.innerHTML = i + " - FizzBuzz";
+      outputEl.appendChild(newEl);
+      console.log("FizzBuzz");
+
+    } else if (i%3 == 0) {
+      newEl.innerHTML = i + " - Fizz";
+      outputEl.appendChild(newEl);
+      console.log("Fizz");
+
+    } else if (i%5 ==0) {
+      newEl.innerHTML = i + " - Buzz";
+      outputEl.appendChild(newEl);
+      console.log("Buzz");
+
+    } else if (i%7 ==0) {
+      newEl.innerHTML = i + " - Boom";
+      outputEl.appendChild(newEl);
+      console.log("Boom");
+
+    } else {
+      newEl.innerHTML = i;
+      outputEl.appendChild(newEl);
+      console.log(i);
+
+
+  }
+}
+
 console.log("There is no multiple of 3, 5, and 7 until the number 105");
 for (var i=1; i<201; i++) {
     console.log(i);
