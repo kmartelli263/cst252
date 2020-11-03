a49959bbc9de840a8733009166cecb5e83fc6efb
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-11-03 10:39:10 -0800

    midterm

--- midterm/js/lab.js
+++ midterm/js/lab.js
@@ -1,38 +1,46 @@
+/*Author: Tavia Eckhart, Sean Brown, Kevin Martelli
+Special thanks to Wes's Lecture Powerpoint, https://codepen.io/P1N2O/pen/pyBNzX
+Created: October 2, 2020
+License: Public Domain*/
+
+
 // we define answers
-    var answers = ["It is certain",
-                   "It is decidedly so",
-                   "Without a doubt",
-                   "Yes - definitely",
-                   "You may rely on it",
-                   "As I see it, yes",
-                   "Most likely",
-                   "Outlook good",
-                   "Yes", "Signs point to yes",
+    var answers = ["Fuck Yeah!",
+                   "If I could marry that I would.",
+                   "Fuck Yeah man! Definitely.",
+                   "Fuck Yeah dude!",
+                   "You betcha!",
+                   "As I appears on Google, yes",
+                   "Ummmm I think so. Lets try and solve it.",
+                   "I think ... Yes.",
+                   "Yes",
+                   "Signs point to yes",
                    "Don't count on it",
-                   "My reply is no",
-                   "My sources say no",
+                   "Fuck no.",
+                   "W3Schools says no.",
                    "Outlook not so good",
                    "Very doubtful",
-                   "Reply hazy, try again",
-                   "Ask again later",
-                   "Better not tell you now",
+                   "That's not so hot, ask me again.",
+                   "Ask me after you've checked the console.",
+                   "I can't give away my secrets just yet.",
                    "Cannot predict now",
-                   "Concentrate and ask again"];
-
+                   "Concentrate and check the console."];
+// When the page loads it initiates the function that calls each element by ID
     window.onload = function() {
        var eight = document.getElementById("eight");
        var answer = document.getElementById("answer");
        var eightball = document.getElementById("eight-ball");
        var question = document.getElementById("question");
-
+// when you click the eightball element trigger a function that takes the length of the input string and checks to see if its less than 2 letters
        eightball.addEventListener("click", function() {
-         if (question.value.length < 1) {
+         if (question.value.length < 2) {
+           //less than two letters prompts the user to enter a question
            alert('Enter a question!');
          } else {
+           //change the eightball's text to a random answers
            eight.innerText = "";
            var num = Math.floor(Math.random() * Math.floor(answers.length));
            answer.innerText = answers[num];
          }
        });
     };
-  
