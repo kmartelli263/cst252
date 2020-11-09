59c981060e80bb3f25ca0f8e8c3c9b52704bd37c
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-11-08 19:44:36 -0800

    lab17

--- lab17/js/lab.js
+++ lab17/js/lab.js
@@ -7,21 +7,21 @@
 
 // The functions below will change the color of the text when you click them.
 
-  // This changes the "Challenge" section to red.
+  // This changes the "Challenge" section to magenta.
   $(document).ready(function(){
     $("#challenge-button").click(function(){
         $("#challenge").css({"color": "#ff00d0"});},
     );
   });
 
-    // This changes the "Problems" section to blue.
+    // This changes the "Problems" section to green.
       $(document).ready(function(){
         $("#problems-button").click(function(){
             $("#problems").css({"color": "#00ff0d"});},
         );
       });
 
-      // This changes the "Results" section to yellow.
+      // This changes the "Results" section to orange.
         $(document).ready(function(){
           $("#results-button").click(function(){
               $("#results").css({"color": "#ff8800"});},
