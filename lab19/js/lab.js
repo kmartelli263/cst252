aa999e6f085763740d73f44eb72b53c92a154335
kmartelli263 <70295036+kmartelli263@users.noreply.github.com>
2020-11-14 12:01:39 -0800

    lab19

---
+++ lab19/js/lab.js
@@ -0,0 +1,37 @@
+/*
+* Author: Kevin Martelli <kmartelli@csumb.edu>
+* Created: 12 November 2020
+* License: Public Domain
+* Credit: Wes Modes and the JS fiddle document that was created in lecture (https://jsfiddle.net/wmodes/mvL786dy/23/)
+*/
+
+
+  endpoint="https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+
+  // add event listener
+$("button").click(function(){
+    	$.ajax({
+          // The URL for the request
+        url: endpoint,
+        // The data to send (will be converted to a query string)
+        data: {
+        	latitude: "45.575",
+          longitude: "-124.565",
+          api_key: "5JNF4JJDJ33J3JH402900"
+        },
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType : "json",
+      })
+      // If the request succeeds
+      .done(function( data ) {
+          // alert("Success!");
+          var quote = data.message;
+          $("#output").append("<p>" + quote)
+      })
+      // If the request fails
+      .fail(function( xhr, status, errorThrown ) {
+          console.log(errorThrown + " Status:" + status );
+    	})
+    })
